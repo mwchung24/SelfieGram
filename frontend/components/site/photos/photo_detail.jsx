6b4c6a54ef6ead1dfbbf5f116923f92da592e471
Martin Chung <mwchung24@gmail.com>
2017-06-24 22:40:41 -0400

    Updated modal css

--- frontend/components/site/photos/photo_detail.jsx
+++ frontend/components/site/photos/photo_detail.jsx
@@ -20,14 +20,32 @@ class PhotoDetail extends React.Component {
             <div className="rightModal">
               <div className="headerModal">
                 <div className="user-profile-modal">
-                  <img className="user-profile-pic" src={this.props.state.user.photo_url}/>
+                  <Link to={`/users/${this.props.state.user.username}`} onClick={ () => this.props.closeModal()}><img className="user-profile-pic" src={this.props.state.user.photo_url}/></Link>
                 </div>
                 <div className="user-username">
-                  <div>{this.props.state.user.username}</div>
+                  <div><Link className="username-link" to={`/users/${this.props.state.user.username}`} onClick={ () => this.props.closeModal()}>{this.props.state.user.username}</Link></div>
                 </div>
               </div>
-              <div className="photo-comments">
-                comments go here
+              <div className="bodyModal">
+                <div className="caption">
+                  <p>
+                    <Link className="username-link-caption"
+                      to={`/users/${this.props.state.user.username}`}
+                      onClick={ () => this.props.closeModal()}>{this.props.state.user.username}
+                    </Link>
+                    <span className="photo-caption">
+                      {this.props.state.photo.caption}
+                    </span>
+                  </p>
+                </div>
+                <section className="like-comment">
+                  <button className="like-button">
+                    <i className="fa fa-heart-o" aria-hidden="true"></i>
+                  </button>
+                  <button className="comment-button">
+                    <i className="fa fa-comment-o" aria-hidden="true"></i>
+                  </button>
+                </section>
               </div>
             </div>
           </div>
--- frontend/components/site/photos/photo_detail_container.js
+++ frontend/components/site/photos/photo_detail_container.js
@@ -1,6 +1,7 @@
 import PhotoDetail from './photo_detail';
 import { connect } from 'react-redux';
 import { fetchPhoto } from '../../../actions/photo_actions';
+import { closeModal } from '../../../actions/modal_actions';
 
 const mapStateToProps = (state) => {
 
@@ -12,6 +13,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = dispatch => {
   return {
     fetchPhoto: (photo) => dispatch(fetchPhoto(photo)),
+    closeModal: () => dispatch(closeModal())
   };
 };
 
