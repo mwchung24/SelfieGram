ff99213a0ee5ad108d2c57c61234acade14b3192
Martin Chung <mwchung24@gmail.com>
2017-08-08 10:57:44 -0400

    Shorted debounce to 300 ms

--- frontend/components/site/header/search.jsx
+++ frontend/components/site/header/search.jsx
@@ -18,7 +18,7 @@ class Search extends React.Component {
     this.handleClick = this.handleClick.bind(this);
     this.handleListClick = this.handleListClick.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
-    this.debouncing = debounce(this.handleSearch, 500);
+    this.debouncing = debounce(this.handleSearch, 300);
   }
 
   focus(boolean) {
